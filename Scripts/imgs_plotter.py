218e8e78763a24502ab09ce66e3312936ff3b902
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-04-28 21:21:47 -0600

    Scripts. ImgsPlotter: Update

--- Scripts/imgs_plotter.py
+++ Scripts/imgs_plotter.py
@@ -1,28 +1,15 @@
 import matplotlib.pyplot as plt
 
-from os import listdir
-from os.path import isfile, join
-
 from scipy import ndimage
 
 
-IMG1_PATH = "1d.png"
-IMG2_PATH = "1o.png"
+IMG1_PATH = "3705.png"
+IMG2_PATH = "3706.png"
 
-def load_img(imgs_dir):
-    filenames = [join(imgs_dir, file) for file in listdir(imgs_dir) \
-                                     if isfile(join(imgs_dir, file))]
-    imgs = [ndimage.imread(img, 'L') for img in filenames]
-    return imgs, filenames
 
 if __name__ == '__main__':
-    img1 = ndimage.imread(IMG1_PATH, 'L')
-    img2 = ndimage.imread(IMG2_PATH, 'L')
-    print((img1 - img2).mean())
-    
-    img1 = img1/img1.max()
-    img2 = img2/img2.max()
-    print((img1 - img2).mean())
+    img1 = ndimage.imread(IMG1_PATH)
+    img2 = ndimage.imread(IMG2_PATH)
     
     plt.subplot(221)
     plt.imshow(img1, cmap='gray')
