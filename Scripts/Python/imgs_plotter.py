e568f1ae0fa04e07fd2765cc4a372ef363c6f721
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-05-11 08:45:05 -0600

    Scripts. ImgsPlotter: v1.0.0

--- Scripts/Python/imgs_plotter.py
+++ Scripts/Python/imgs_plotter.py
@@ -3,15 +3,25 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 
 
-IMG1_PATH = "3705.png"
-IMG2_PATH = "3706.png"
+IMG1_PATH = "1.png"
+IMG2_PATH = "gt.png"
+IMG3_PATH = "ausm1.png"
+IMG4_PATH = "ausm2.png"
 
 
 if __name__ == '__main__':
     img1 = ndimage.imread(IMG1_PATH)
     img2 = ndimage.imread(IMG2_PATH)
+    img3 = ndimage.imread(IMG3_PATH)
+    img4 = ndimage.imread(IMG4_PATH)
     
     plt.subplot(221)
     plt.imshow(img1, cmap='gray')
     plt.subplot(222)
     plt.imshow(img2, cmap='gray')
+    plt.subplot(223)
+    plt.imshow(img3, cmap='gray')
+    plt.subplot(224)
+    plt.imshow(img4, cmap='gray')
+    
+    plt.savefig('plot.png')
